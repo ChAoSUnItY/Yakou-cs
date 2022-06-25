f03b571ddd4c865991ddf335ef521070941d808d
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-06-25 21:39:24 +0800

    Just another commit yet ;)

--- src/lexer/Lexer.cs
+++ src/lexer/Lexer.cs
@@ -1,8 +1,15 @@
+using System.Collections.Generic;
+
 namespace org.yakou.lang {
     public class Lexer {
-        private int pos { get; }
+        private readonly string OriginalString;
+        private int line { get; }
+        private int Pos { get; }
         
         public Lexer() {
+        }
+
+        public void lex(string originalString) {
             
         }
     }
