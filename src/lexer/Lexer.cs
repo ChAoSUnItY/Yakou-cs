5cc091fc039583d36338d1e7795fb8c4186dcb4b
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-06-28 02:53:17 +0800

    Complete basic concept of Lexer

--- src/lexer/Lexer.cs
+++ src/lexer/Lexer.cs
@@ -2,15 +2,30 @@ using System.Collections.Generic;
 
 namespace org.yakou.lang {
     public class Lexer {
-        private readonly string? OriginalString;
-        private int line { get; }
-        private int Pos { get; }
+        private List<string>? OriginalString { get; set; }
+        private string? CurrentString;
+        private int Line { get; set; }
+        private int Pos { get; set; }
         
         public Lexer() {
         }
 
-        public void lex(string originalString) {
-            
+        public void lex(List<string> originalString)
+        {
+            OriginalString = originalString;
+
+            while (Line < OriginalString.Count)
+            {
+                CurrentString = OriginalString[Line];
+
+                while (Pos < CurrentString.Length)
+                {
+                    // Lex stuff here 
+                }
+            }
+
+            Pos = 0;
+            Line++;
         }
     }
 }
